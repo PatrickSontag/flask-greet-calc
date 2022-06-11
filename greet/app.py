4fe2b8eb5970575ae82ff6dee79e3a23b82d845f
Patrick Sontag <pmsontag84@gmail.com>
2022-06-11 10:00:20 -0500

    calc functions

---
+++ greet/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+# , request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    html = """
+    <html>
+        <body>
+            <h1>Flask</h1>
+            <p>Greet</p>
+        </body>
+    </html>
+    """
+    return html
+
+@app.route('/welcome')
+def welcome():
+    return "welcome"
+@app.route('/welcome/back')
+def welcome_back():
+    return "welcome back"
+@app.route('/welcome/home')
+def welcome_home():
+    return "welcome home"
